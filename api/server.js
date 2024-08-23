2c914256bd7f5bc784ab79991a2e48584d343be5
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-23 09:45:29 -0400

    GET users endpoint working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,27 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', (req, res) => {
+   User.find()
+       .then(users => {
+        res.json(users)
+       })
+       .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+        })
+       })
+})
+
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'Not found'
+    })
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
